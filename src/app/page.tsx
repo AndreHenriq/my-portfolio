4dc4975f3d69ffeca6d3a1a7af97fe26e019f3eb
Andre <andrecomar5@gmail.com>
2024-01-08 13:51:06 -0300

    feat: information section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Experience } from './components/experience/experience'
 import Header from './components/header/header'
+import{Info} from './components/information/information'
 import './styles/home.scss'
 
 export default function Home() {
@@ -7,18 +8,7 @@ export default function Home() {
     <main className='container'>
       <Header/>
       <Experience/>
-        <div className='infos'>
-          <h3>Languages</h3>
-          <div className='languages-info'>
-            <span>🇺🇸 EN - Fluent</span>
-            <span>🇧🇷 PT-BR - Native</span>
-          </div>
-          <h3>Education</h3>
-          <div className='educational-info'>
-            <span>🎓</span>
-            <span>Systems Development and Analysis - Federal Institute of Paraná</span>
-          </div>
-        </div>
+      <Info/>
         <div className='buttons'>
         <div className='social'></div>
         <button>Contact me!</button>
